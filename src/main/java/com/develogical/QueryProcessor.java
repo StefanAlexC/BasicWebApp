73a5079297251673dd66002220b81c161fbe4134
Alex <sc3316@ic.ac.uk>
2020-01-21 11:29:15 +0000

    team

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -1,22 +1,31 @@
 package com.develogical;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
-                    "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.toLowerCase().contains("milk")) {
-            return "Please mister gib milk";
-        } else if (query.toLowerCase().contains(" what is your team name")) {
-            return "Milky Way";
-        } else if (query.toLowerCase().contains("plus")) {
-            Scanner scanner = new Scanner(query.toLowerCase());
-            return "" + scanner.nextInt() + scanner.nextInt();
+        try {
+            if (query.toLowerCase().contains("shakespeare")) {
+                return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+                        "English poet, playwright, and actor, widely regarded as the greatest " +
+                        "writer in the English language and the world's pre-eminent dramatist.";
+            } else if (query.toLowerCase().contains("milk")) {
+                return "Please mister gib milk";
+            } else if (query.toLowerCase().contains(" what is your team name")) {
+                return "Milky Way";
+            } else if (query.toLowerCase().contains("plus")) {
+                Scanner scanner = new Scanner(query.toLowerCase());
+                return "" + scanner.nextInt() + scanner.nextInt();
+            } else if (query.toLowerCase().contains("largest")) {
+                Scanner scanner = new Scanner(query.toLowerCase());
+                int max = 0;
+
+            }
+            return "";
+        } catch (Exception e) {
+            return "";
         }
-        return "";
     }
 }
